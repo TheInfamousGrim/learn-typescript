99a98e5423df65f2cfe28dfe74c03ef43a0e802a
TheInfamousGrim <finchergeorge1@gmail.com>
2023-03-08 22:48:19 +0000

    Feat 🎯: Create first TypeScript program
    Created my first TypeScript program that will greet an individual with
    the date specified.

---
+++ Handbook/01-The-Basics/04-tsc-TypeScript-Compiler/src/hello.ts
@@ -0,0 +1,7 @@
+// Greets the world.
+// This is an industrial-grade general-purpose greeter function:
+function greet(person: string, date: string) {
+  console.log(`Hello ${person}, today is ${date}!`);
+}
+
+greet("Brendan", "08/03/2023");
